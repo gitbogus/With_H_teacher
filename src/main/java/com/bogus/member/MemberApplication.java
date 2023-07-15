0a9b0748f0eaa9eb48349ead499d00459e374f83
gitbogus <bogus0115@naver.com>
2023-07-15 18:31:55 +0900

    first commit

---
+++ src/main/java/com/bogus/member/MemberApplication.java
@@ -0,0 +1,19 @@
+package com.bogus.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberApplication.class, args);
+	}
+
+
+}
+
+
+
+

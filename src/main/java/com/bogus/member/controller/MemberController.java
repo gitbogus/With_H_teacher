d10a40ee80a5250f0ada7770aac303a52efe0330
gitbogus <bogus0115@naver.com>
2023-07-18 20:58:34 +0900

    Second_HomeStudy

--- src/main/java/com/bogus/member/controller/MemberController.java
+++ src/main/java/com/bogus/member/controller/MemberController.java
@@ -4,10 +4,13 @@ import com.bogus.member.dto.MemberDTO;
 import com.bogus.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,8 +28,30 @@ public class MemberController {
     public String save(@ModelAttribute MemberDTO memberDTO) {
         System.out.println("MemberController.save");
         System.out.println("memberDTO = " + memberDTO);
-        MemberService memberService = new MemberService();
         memberService.save(memberDTO);
         return "index";
+    }
+    @GetMapping("/member/login")
+    public String loginForm() {
+        return "login";
+    }
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if (loginResult != null) {
+            // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return  "main";
+        } else {
+            // login 실패
+            return "login";
+        }
+    }
+    @GetMapping("/member/")
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        // 어떠한 html로 가져갈 데이터가 있다면 model사용
+        model.addAttribute("memberList", memberDTOList);
+        return "list";
     }
 }
